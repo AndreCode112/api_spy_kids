069accf857d07274c2f3b3a4c655c34970927643
AndreCode112 <andremartinsdias2019@gmail.com>
2026-01-05 01:16:50 -0300

    alterações para deploy statics

--- main/settings.py
+++ main/settings.py
@@ -134,9 +134,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'videos'), 
+    ('videos', os.path.join(BASE_DIR, 'videos')), 
 ]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
