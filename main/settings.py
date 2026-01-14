8015fb85013313a4d36b60cad8b1bced1b8705d2
AndreCode112 <andremartinsdias2019@gmail.com>
2026-01-14 12:19:46 -0300

    preparando para produção

--- main/settings.py
+++ main/settings.py
@@ -129,18 +129,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # opcional (para arquivos globais)
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # ONDE O DJANGO VAI PROCURAR OS ARQUIVOS (A pasta da Imagem 2)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+#     BASE_DIR / 'static',  # opcional (para arquivos globais)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
